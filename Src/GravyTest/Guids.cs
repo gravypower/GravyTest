daa2b14643e742d9922eca0959131e380a81ce1b
gravypower <aaron@gravypower.net>
2013-11-07 16:58:48 +0000

    Playing around with an idea for a VS extension

---
+++ Src/GravyTest/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Gravypowered.GravyTest
+{
+    static class GuidList
+    {
+        public const string guidGravyTestPkgString = "245666e7-b4e2-4a39-a349-f4b5c218974a";
+        public const string guidGravyTestCmdSetString = "547938d2-b887-4d91-92ac-b6563c9d9e88";
+
+        public static readonly Guid guidGravyTestCmdSet = new Guid(guidGravyTestCmdSetString);
+    };
+}
---
+++ Src/GravyTest/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Gravypowered.GravyTest
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidAddWhenCase =        0x100;
+
+
+    };
+}
